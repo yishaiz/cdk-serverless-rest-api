d43732c4e001d9a549bc7e4267e405efc6043595
yishai <yishai5555@gmail.com>
2025-07-03 18:01:38 +0300

    02-simple-example

--- py-rest-api/services/index.py
+++ py-rest-api/services/index.py
@@ -9,38 +9,43 @@ table = dynamodb.Table(table_name)
 
 
 def handler(event, context):
-    method = event["httpMethod"]
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"Hello !"),
+    }
+    
+    # method = event["httpMethod"]
 
-    if method == "POST":
-        item = json.loads(event["body"])
-        item["id"] = str(uuid.uuid4())
-        table.put_item(Item=item)
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"id": item["id"]}),
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "*",
-            },
-        }
-    if method == "GET":
-        empl_id = event["queryStringParameters"]["id"]
-        response = table.get_item(Key={"id": empl_id})
-        if "Item" in response:
-            return {
-                "statusCode": 200,
-                "body": json.dumps(response["Item"]),
-                "headers": {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "*",
-                },
-            }
-        else:
-            return {
-                "statusCode": 404,
-                "body": json.dumps("Not found"),
-                "headers": {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "*",
-                },
-            }
+    # if method == "POST":
+    #     item = json.loads(event["body"])
+    #     item["id"] = str(uuid.uuid4())
+    #     table.put_item(Item=item)
+    #     return {
+    #         "statusCode": 200,
+    #         "body": json.dumps({"id": item["id"]}),
+    #         "headers": {
+    #             "Access-Control-Allow-Origin": "*",
+    #             "Access-Control-Allow-Methods": "*",
+    #         },
+    #     }
+    # if method == "GET":
+    #     empl_id = event["queryStringParameters"]["id"]
+    #     response = table.get_item(Key={"id": empl_id})
+    #     if "Item" in response:
+    #         return {
+    #             "statusCode": 200,
+    #             "body": json.dumps(response["Item"]),
+    #             "headers": {
+    #                 "Access-Control-Allow-Origin": "*",
+    #                 "Access-Control-Allow-Methods": "*",
+    #             },
+    #         }
+    #     else:
+    #         return {
+    #             "statusCode": 404,
+    #             "body": json.dumps("Not found"),
+    #             "headers": {
+    #                 "Access-Control-Allow-Origin": "*",
+    #                 "Access-Control-Allow-Methods": "*",
+    #             },
+    #         }
