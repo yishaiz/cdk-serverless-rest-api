164c1f3e955774624a7aef3966f74458ef9af582
yishai <yishai5555@gmail.com>
2025-07-03 19:45:15 +0300

    03-ts

--- ts-rest-api/lib/ts-rest-api-stack.ts
+++ ts-rest-api/lib/ts-rest-api-stack.ts
@@ -3,47 +3,50 @@ import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
-import { Cors, LambdaIntegration, ResourceOptions, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import {
+  Cors,
+  LambdaIntegration,
+  ResourceOptions,
+  RestApi,
+} from 'aws-cdk-lib/aws-apigateway';
 import { AttributeType, Billing, TableV2 } from 'aws-cdk-lib/aws-dynamodb';
 
 export class TsRestApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const employeesTable = new TableV2(this, 'TS-EmplTable', {
-      partitionKey: {
-        name: 'id',
-        type: AttributeType.STRING
-      },
-      billing: Billing.onDemand()     
-    });
+    // const employeesTable = new TableV2(this, 'TS-EmplTable', {
+    //   partitionKey: {
+    //     name: 'id',
+    //     type: AttributeType.STRING
+    //   },
+    //   billing: Billing.onDemand()
+    // });
 
     const emplLambda = new NodejsFunction(this, 'Ts-EmplLambda', {
       runtime: Runtime.NODEJS_18_X,
       handler: 'handler',
-      entry: (join(__dirname, '..', 'services', 'handler.ts')),
-      environment: {
-        TABLE_NAME: employeesTable.tableName
-      },
-    })
+      entry: join(__dirname, '..', 'services', 'handler.ts'),
+    });
+    // environment: {
+    //   TABLE_NAME: employeesTable.tableName
+    // },
 
-    employeesTable.grantReadWriteData(emplLambda);
+    // employeesTable.grantReadWriteData(emplLambda);
 
+    //   const optionsWithCors: ResourceOptions = {
+    //     defaultCorsPreflightOptions: {
+    //         allowOrigins: Cors.ALL_ORIGINS,
+    //         allowMethods: Cors.ALL_METHODS
+    //     }
+    // }
     const api = new RestApi(this, 'TS-EmplApi');
-
-    const optionsWithCors: ResourceOptions = {
-      defaultCorsPreflightOptions: {
-          allowOrigins: Cors.ALL_ORIGINS,
-          allowMethods: Cors.ALL_METHODS
-      }
-  }
-    const emplResource = api.root.addResource('empl', optionsWithCors);
+    const emplResource = api.root.addResource('empl');
 
     const emplLambdaIntegration = new LambdaIntegration(emplLambda);
 
     emplResource.addMethod('GET', emplLambdaIntegration);
     emplResource.addMethod('POST', emplLambdaIntegration);
-
-
   }
 }
+// const emplResource = api.root.addResource('empl', optionsWithCors);
--- ts-rest-api/services/handler.ts
+++ ts-rest-api/services/handler.ts
@@ -1,38 +1,48 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
-import { postEmpl } from "./PostEmpl";
-import { getEmpl } from "./GetEmpl";
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import {
+  APIGatewayProxyEvent,
+  type APIGatewayProxyResult,
+  Context,
+} from 'aws-lambda';
+import { postEmpl } from './PostEmpl';
+import { getEmpl } from './GetEmpl';
 
 const ddbClient = new DynamoDBClient({});
 
-async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+async function handler(
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+  return {
+    statusCode: 400,
+    body: JSON.stringify('Hello from Lambda !'),
+  };
+  //     let response: APIGatewayProxyResult = {} as any;
 
-    let response: APIGatewayProxyResult = {} as any;
+  //   try {
+  //     switch (event.httpMethod) {
+  //       case 'GET':
+  //         const getResponse = await getEmpl(event, ddbClient);
+  //         response = getResponse;
+  //         break;
+  //       case 'POST':
+  //         const postResponse = await postEmpl(event, ddbClient);
+  //         response = postResponse;
+  //         break;
+  //     }
+  //   } catch (error) {
+  //     response = {
+  //       statusCode: 400,
+  //       body: JSON.stringify(error.message),
+  //     };
+  //   }
 
-    try {
-        switch (event.httpMethod) {
-            case 'GET':
-                const getResponse = await getEmpl(event, ddbClient);
-                response = getResponse;
-                break;
-            case 'POST':
-                const postResponse = await postEmpl(event, ddbClient);
-                response = postResponse;
-                break;
-        }
-    } catch (error) {
-        response = {
-            statusCode: 400,
-            body: JSON.stringify(error.message)
-        }
-    }
+  //   response.headers = {
+  //     'Access-Control-Allow-Origin': '*',
+  //     'Access-Control-Allow-Methods': '*',
+  //   };
 
-    response.headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': '*'
-    }
-
-    return response;
+  //   return response;
 }
 
-export { handler }
+export { handler };
